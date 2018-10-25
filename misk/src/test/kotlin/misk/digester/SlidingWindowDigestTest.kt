fceb6f97d7c628d87798f23a79e0e92d22eb3eb9
Daniel Weisberg <dweisberg@squareup.com>
2018-10-25 12:03:28 -0700

    Refactored

--- misk/src/test/kotlin/misk/digester/SlidingWindowDigestTest.kt
+++ misk/src/test/kotlin/misk/digester/SlidingWindowDigestTest.kt
@@ -9,27 +9,26 @@ import java.util.SortedMap
 import java.util.concurrent.TimeUnit
 
 class SlidingWindowDigestTest {
-  private val baseClock: FakeClock = FakeClock()
 
   @Test
   fun slidingWindowDigestEmpty() {
-    val digest = newSlidingWindowDigestTest()
-    assertThat(digest.quantile(0.5)).isEqualTo(Double.NaN)
-    expectQuantiles(digest, 0, Double.NaN, sortedMapOf())
-    advanceWindows(1, digest)
-    assertThat(digest.quantile(0.5)).isEqualTo(Double.NaN)
-    expectQuantiles(digest, 0, Double.NaN, sortedMapOf())
+    val digest = SlidingWindowDigestTestingSuite()
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(Double.NaN)
+    digest.expectQuantiles(0, Double.NaN, sortedMapOf())
+    digest.advanceWindows(1)
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(Double.NaN)
+    digest.expectQuantiles(0, Double.NaN, sortedMapOf())
   }
 
   @Test
   fun slidingWindowDigestObservation() {
-    val digest = newSlidingWindowDigestTest()
-    digest.observe(10.0)
-    digest.observe(20.0)
-    digest.observe(30.0)
-    val windows = windows(digest)
+    val digest = SlidingWindowDigestTestingSuite()
+    digest.slidingWindowDigest.observe(10.0)
+    digest.slidingWindowDigest.observe(20.0)
+    digest.slidingWindowDigest.observe(30.0)
+    val windows = digest.windows()
     expectWindowDigests(
-        digest.windows.toList(),
+        digest.slidingWindowDigest.windows.toList(),
         listOf(
             newWindowDigest(windows[0], listOf(10.0, 20.0, 30.0)),
             newWindowDigest(windows[1], listOf(10.0, 20.0, 30.0)),
@@ -37,11 +36,11 @@ class SlidingWindowDigestTest {
         )
     )
     // No windows have closed yet so there is no reportable data yet
-    assertThat(digest.quantile(0.5)).isEqualTo(Double.NaN)
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(Double.NaN)
     // Advance time so that one window is now closed
-    advanceWindows(1, digest)
-    assertThat(digest.quantile(0.5)).isEqualTo(30.0)
-    expectQuantiles(digest, 3, 60.0,
+    digest.advanceWindows(1)
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(30.0)
+    digest.expectQuantiles(3, 60.0,
         sortedMapOf(
             0.25 to 30.0,
             0.5 to 30.0))
@@ -49,19 +48,19 @@ class SlidingWindowDigestTest {
 
   @Test
   fun slidingWindowDigestObservationInMultipleWindows() {
-    val digest = newSlidingWindowDigestTest()
-    val windowsT0_2 = windows(digest)
-    digest.observe(10.0) // in t0-t2 buckets
-    advanceWindows(1, digest)
-    digest.observe(20.0) // in t1-t3 buckets
-    assertThat(digest.quantile(0.5)).isEqualTo(10.0)
-    advanceWindows(1, digest)
-    digest.observe(30.0) // in t2-t4 buckets
-    assertThat(digest.quantile(0.5)).isEqualTo(20.0)
-    val windowsT3_5 = advanceWindows(1, digest)
-    assertThat(digest.quantile(0.5)).isEqualTo(30.0)
+    val digest = SlidingWindowDigestTestingSuite()
+    val windowsT0_2 = digest.windows()
+    digest.slidingWindowDigest.observe(10.0) // in t0-t2 buckets
+    digest.advanceWindows(1)
+    digest.slidingWindowDigest.observe(20.0) // in t1-t3 buckets
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(10.0)
+    digest.advanceWindows(1)
+    digest.slidingWindowDigest.observe(30.0) // in t2-t4 buckets
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(20.0)
+    val windowsT3_5 = digest.advanceWindows(1)
+    assertThat(digest.slidingWindowDigest.quantile(0.5)).isEqualTo(30.0)
     expectWindowDigests(
-        digest.windows,
+        digest.slidingWindowDigest.windows,
         listOf(
             newWindowDigest(windowsT0_2[0], listOf(10.0)),
             newWindowDigest(windowsT0_2[1], listOf(10.0, 20.0)),
@@ -73,47 +72,52 @@ class SlidingWindowDigestTest {
 
   @Test
   fun slidingWindowDigestClosedDigests() {
-    val digest = newSlidingWindowDigestTest()
-    val windows = windows(digest)
-    digest.observe(10.0)
-    expectWindowDigests(digest.closedDigests(windows[2].end.plusNanos(1)), listOf())
-    expectWindowDigests(digest.closedDigests(windows[2].end), listOf(
+    val digest = SlidingWindowDigestTestingSuite()
+    val windows = digest.windows()
+    digest.slidingWindowDigest.observe(10.0)
+    expectWindowDigests(digest.slidingWindowDigest.closedDigests(windows[2].end.plusNanos(1)),
+        listOf())
+    expectWindowDigests(digest.slidingWindowDigest.closedDigests(windows[2].end), listOf(
         newWindowDigest(windows[2], listOf(10.0))
     ))
-    expectWindowDigests(digest.closedDigests(windows[1].end.plusNanos(1)), listOf(
-        newWindowDigest(windows[2], listOf(10.0))
-    ))
-    expectWindowDigests(digest.closedDigests(windows[1].end), listOf(
+    expectWindowDigests(digest.slidingWindowDigest.closedDigests(windows[1].end.plusNanos(1)),
+        listOf(
+            newWindowDigest(windows[2], listOf(10.0))
+        ))
+    expectWindowDigests(digest.slidingWindowDigest.closedDigests(windows[1].end), listOf(
         newWindowDigest(windows[1], listOf(10.0)),
         newWindowDigest(windows[2], listOf(10.0))
     ))
-    expectWindowDigests(digest.closedDigests(windows[0].end.minusNanos(0)), listOf(
-        newWindowDigest(windows[0], listOf(10.0)),
-        newWindowDigest(windows[1], listOf(10.0)),
-        newWindowDigest(windows[2], listOf(10.0))
-    ))
+    expectWindowDigests(digest.slidingWindowDigest.closedDigests(windows[0].end.minusNanos(0)),
+        listOf(
+            newWindowDigest(windows[0], listOf(10.0)),
+            newWindowDigest(windows[1], listOf(10.0)),
+            newWindowDigest(windows[2], listOf(10.0))
+        ))
   }
 
   @Test
   fun slidingWindowDigestMergeInEmptyToEmpty() {
-    val src = newSlidingWindowDigestTest()
-    val dest = newSlidingWindowDigestTest()
-    advanceWindows(3, src)
-    advanceWindows(3, dest)
-    dest.mergeIn(
-        src.closedDigests(ZonedDateTime.ofInstant(baseClock.instant(), ZoneId.of("UTC"))))
-    assertThat(dest.windows.count()).isEqualTo(0)
+    val src = SlidingWindowDigestTestingSuite()
+    val dest = SlidingWindowDigestTestingSuite()
+    src.advanceWindows(3)
+    dest.advanceWindows(3)
+    dest.slidingWindowDigest.mergeIn(
+        src.slidingWindowDigest.closedDigests(
+            ZonedDateTime.ofInstant(src.baseClock.instant(), ZoneId.of("UTC"))))
+    assertThat(dest.slidingWindowDigest.windows.count()).isEqualTo(0)
   }
 
   @Test
   fun slidingWindowDigestMergeInEmptyToValues() {
-    val src = newSlidingWindowDigestTest()
-    val dest = newSlidingWindowDigestTest()
-    val windowsT0_2 = windows(dest)
-    dest.observe(10.0)
-    dest.mergeIn(
-        src.closedDigests(ZonedDateTime.ofInstant(baseClock.instant(), ZoneId.of("UTC"))))
-    expectWindowDigests(dest.windows, listOf(
+    val src = SlidingWindowDigestTestingSuite()
+    val dest = SlidingWindowDigestTestingSuite()
+    val windowsT0_2 = dest.windows()
+    dest.slidingWindowDigest.observe(10.0)
+    dest.slidingWindowDigest.mergeIn(
+        src.slidingWindowDigest.closedDigests(
+            ZonedDateTime.ofInstant(src.baseClock.instant(), ZoneId.of("UTC"))))
+    expectWindowDigests(dest.slidingWindowDigest.windows, listOf(
         newWindowDigest(windowsT0_2[0], listOf(10.0)),
         newWindowDigest(windowsT0_2[1], listOf(10.0)),
         newWindowDigest(windowsT0_2[2], listOf(10.0))
@@ -122,20 +126,18 @@ class SlidingWindowDigestTest {
 
   @Test
   fun slidingWindowDigestMergeInValuesToValues() {
-    val srcClock = FakeClock()
-    val destClock = FakeClock()
-    val src = newSlidingWindowDigestTest(srcClock)
-    val dest = newSlidingWindowDigestTest(destClock)
-    val windowsT0_2 = windows(src, srcClock)
-    src.observe(100.0) // in t0-t2 buckets
-    val windowsT3_5 = advanceWindows(3, src, srcClock)
-    src.observe(200.0) // in t3-t5 buckets
-    advanceWindows(3, src, srcClock)
-    advanceWindows(1, dest, destClock)
-    dest.observe(10.0) // in t1-t3 buckets
-    assertThat(dest.openDigests(false).count()).isEqualTo(3)
-    dest.mergeIn(src.closedDigests(windowsT0_2[0].end))
-    expectWindowDigests(dest.windows, listOf(
+    val src = SlidingWindowDigestTestingSuite()
+    val dest = SlidingWindowDigestTestingSuite()
+    val windowsT0_2 = src.windows()
+    src.slidingWindowDigest.observe(100.0) // in t0-t2 buckets
+    val windowsT3_5 = src.advanceWindows(3)
+    src.slidingWindowDigest.observe(200.0) // in t3-t5 buckets
+    src.advanceWindows(3)
+    dest.advanceWindows(1)
+    dest.slidingWindowDigest.observe(10.0) // in t1-t3 buckets
+    assertThat(dest.slidingWindowDigest.openDigests(false).count()).isEqualTo(3)
+    dest.slidingWindowDigest.mergeIn(src.slidingWindowDigest.closedDigests(windowsT0_2[0].end))
+    expectWindowDigests(dest.slidingWindowDigest.windows, listOf(
         newWindowDigest(windowsT0_2[0], listOf(100.0)),
         newWindowDigest(windowsT0_2[1], listOf(10.0, 100.0)),
         newWindowDigest(windowsT0_2[2], listOf(10.0, 100.0)),
@@ -147,54 +149,26 @@ class SlidingWindowDigestTest {
 
   @Test
   fun slidingWindowDigestGC() {
-    val digest = newSlidingWindowDigestTest()
-    digest.observe(10.0)
+    val digest = SlidingWindowDigestTestingSuite()
+    digest.slidingWindowDigest.observe(10.0)
     // Move just past the threshold for collecting the last window
-    baseClock.setNow(windows(digest)[2].end.toInstant())
-    baseClock.add(1, TimeUnit.MINUTES)
-    baseClock.add(1, TimeUnit.MILLISECONDS)
+    digest.baseClock.setNow(digest.windows()[2].end.toInstant())
+    digest.baseClock.add(1, TimeUnit.MINUTES)
+    digest.baseClock.add(1, TimeUnit.MILLISECONDS)
 
-    digest.observe(20.0)
-    val windows = windows(digest)
-    expectWindowDigests(digest.windows, listOf(
+    digest.slidingWindowDigest.observe(20.0)
+    val windows = digest.windows()
+    expectWindowDigests(digest.slidingWindowDigest.windows, listOf(
         newWindowDigest(windows[0], listOf(20.0)),
         newWindowDigest(windows[1], listOf(20.0)),
         newWindowDigest(windows[2], listOf(20.0))
     ))
   }
 
-  fun setClock(t: ZonedDateTime, clock: FakeClock) {
-    require(!t.toInstant().isBefore(clock.instant())) {
-      "Cannot go back in time"
-    }
-
-    clock.setNow(t.toInstant())
-  }
-
-  fun windows(slidingWindow: SlidingWindowDigest<FakeDigest>, clock: FakeClock = baseClock): List<Window> {
-    return slidingWindow.windower.windowsContaining(
-        ZonedDateTime.ofInstant(clock.instant(), ZoneId.of("UTC")))
-  }
-
-  fun advanceWindows(n: Int, digest: SlidingWindowDigest<FakeDigest>, clock: FakeClock = baseClock): List<Window> {
-    repeat(n) {
-      setClock(windows(digest, clock)[0].end, clock)
-    }
-    return windows(digest, clock)
-  }
-
   fun newWindowDigest(window: Window, values: List<Double>): WindowDigest<FakeDigest> {
     return WindowDigest(
         window,
         FakeDigest(values)
-    )
-  }
-
-  fun newSlidingWindowDigestTest(clock: FakeClock = baseClock): SlidingWindowDigest<FakeDigest> {
-    return SlidingWindowDigest(
-        Windower(10, 3),
-        fun() = FakeDigest(),
-        clock
     )
   }
 
@@ -213,14 +187,44 @@ class SlidingWindowDigestTest {
       assertThat(actual[i].digest.addedValues).isEqualTo(expected[i].digest.addedValues)
     }
   }
+}
+
+class SlidingWindowDigestTestingSuite {
+  val baseClock: FakeClock = FakeClock()
+
+  val slidingWindowDigest = SlidingWindowDigest(
+      Windower(10, 3),
+      fun() = FakeDigest(),
+      baseClock
+  )
+
+  fun setClock(t: ZonedDateTime) {
+    require(!t.toInstant().isBefore(baseClock.instant())) {
+      "Cannot go back in time"
+    }
+
+    baseClock.setNow(t.toInstant())
+  }
+
+  fun windows(): List<Window> {
+    return slidingWindowDigest.windower.windowsContaining(
+        ZonedDateTime.ofInstant(baseClock.instant(), ZoneId.of("UTC")))
+  }
+
+  fun advanceWindows(n: Int): List<Window> {
+    repeat(n) {
+      setClock(windows()[0].end)
+    }
+    return windows()
+  }
 
   fun expectQuantiles(
-    digest: SlidingWindowDigest<FakeDigest>,
     count: Long,
     sum: Double,
     quantileVals: SortedMap<Double, Double>
   ) {
-    val snapshot = digest.snapshot(quantileVals.keys.toList()) //should this be keys or values?
+    val snapshot =
+        slidingWindowDigest.snapshot(quantileVals.keys.toList()) //should this be keys or values?
     assertThat(snapshot.count).isEqualTo(count)
     assertThat(snapshot.sum).isEqualTo(sum)
     assertThat(snapshot.quantileVals.toDoubleArray()).isEqualTo(quantileVals.values.toDoubleArray())
